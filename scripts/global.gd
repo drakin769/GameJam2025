81b117b2b83b94611f93037be8cbcb9d377057f2
moop <katycatt6@gmail.com>
2025-08-02 18:30:03 +0200

    added functions for adding/nuking animated text

--- scripts/global.gd
+++ scripts/global.gd
@@ -11,3 +11,17 @@ func roll():
 	print("ROLLED A "+ str(holder))
 	fate_number += 1
 	return holder
+
+
+## Will add a new line of text to Dot matrix component.
+## To add multiple lines, call this multiple times.
+signal dot_matrix_add_text(new_text: String)
+func add_text_to_dot_matrix(new_text: String):
+	print("triger global text add")
+	dot_matrix_add_text.emit(new_text)
+	
+## Will nuke all text from Dot matrix component.
+signal dot_matrix_nuke_text()
+func nuke_dot_matrix():
+	print("triger global text nuke")
+	dot_matrix_nuke_text.emit()
