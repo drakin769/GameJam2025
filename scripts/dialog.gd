de929424266ea17995ddbdd0d1b30bff77e11b6c
FDNinja <drakin769@gmail.com>
2025-08-01 22:17:44 -0400

    Big Update: Core mechanics now work and will enable interactions to chain. Moved most of the script from main to Dialog.gd to solve some scope issue easier.

--- scripts/dialog.gd
+++ scripts/dialog.gd
@@ -1,9 +1,20 @@
 extends Control
-
-
+var roll_result = 0
+var interactions = JSON.new()
+func _ready():
+	interactions=file_load()
+	load_interaction("first")
 func _on_dialog_text_meta_clicked(meta):
-	print("I HAVE BEEN Poked ")
-	print(str(meta))
+	if meta.left(4) == "Roll" :
+		roll_result=global.roll()
+		if roll_result >= interactions[meta].DC:
+			load_interaction(meta+"PASS")
+		elif roll_result>(interactions[meta].DC-9):
+			load_interaction(meta+"FAIL")
+		else:
+			load_interaction(meta+"BOTCH")
+	else:
+		load_interaction(meta)
 	
 func file_load():
 	var file = FileAccess.open("res://data/interactions.json", FileAccess.READ)
@@ -11,10 +22,25 @@ func file_load():
 	return content
 
 func change_speaker(speaker):#Changes the dialog window colors and image to selected speaker
-	%SpeakerText.text=""#new speaker
+	%SpeakerText.text=speaker
 	pass
 func clear_text():
 	%SpeakerText.text=""
 	%DialogText.text=""
 func set_text(string):
 	%DialogText.text=string #can be made beter with 
+func hide_dialog():
+	pass#
+func show_dialog():
+	pass#
+func load_interaction(current_interaction):
+	hide_dialog()
+	clear_text()
+	change_speaker(interactions[current_interaction].speaker)
+	set_text(interactions[current_interaction].text[interactions[current_interaction].version])
+	show_dialog()
+	for n in interactions[current_interaction].change:
+		print(interactions[n].version)
+		print(interactions[n].text.size())
+		if int(interactions[n].version)+1 < int(interactions[n].text.size()):
+			interactions[n].version += 1
--- scripts/main.gd
+++ scripts/main.gd
@@ -1,8 +1,9 @@
 extends Node2D
-var interactions = JSON.new()
+var test
 func _ready():
-		global.roll()
-		interactions=%DialogUI.file_load()
+	pass
 func reset():
 	global.fate_number=0
 	global.holder = 0
+	global.current_interaction = "first"
+#alas poor main, moved things over to dialog to make scope easier to deal with
